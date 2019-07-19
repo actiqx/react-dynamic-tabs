17799fec5907c5eafdcb81a6ac79660271830853
actiqx <priyatoshpatra@gmail.com>
2019-07-19 14:14:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-7csrzi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7csrzi)
---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+     <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={App} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </BrowserRouter>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
